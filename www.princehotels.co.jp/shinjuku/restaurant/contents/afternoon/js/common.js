29ddfd49265e0b43af8df6899f12a74597e31c7b
hoangf08 <hoangf08@outlook.com>
2025-03-14 18:03:25 +0700

    CNCTR-3504 【新宿】Web修正 -新宿プリンスホテル-Rec08HKQKRB9R, source

---
+++ www.princehotels.co.jp/shinjuku/restaurant/contents/afternoon/js/common.js
@@ -0,0 +1,100 @@
+// スワイパー１
+const FirstSwiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+   
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets'
+    },
+   
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    autoplay: {
+      delay: 4000,
+      pauseOnMouseEnter: true,        // 追記
+      disableOnInteraction: false,  
+  },
+  speed: 500,  
+  });
+// スワイパー2
+  const SecondSwiper = new Swiper('.swiper2', {
+    // Optional parameters
+    loop: true,
+   
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets'
+    },
+   
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    autoplay: {
+      delay: 4000,
+      pauseOnMouseEnter: true,        // 追記
+      disableOnInteraction: false,  
+  },
+  speed: 500,  
+  });
+
+// スワイパー3
+const ThirdSwiper = new Swiper('.swiper3', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 2, // コンテナ内に表示させるスライド数（CSSでサイズ指定する場合は 'auto'）
+    breakpoints: {
+        500: {
+            slidesPerView: 3,
+        },
+        900: {
+            slidesPerView: 4,
+        }
+    },
+    spaceBetween: 17,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets'
+    },
+   
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    autoplay: {
+      delay: 3000,
+      pauseOnMouseEnter: true,        // 追記
+      disableOnInteraction: false,  
+  },
+  speed: 1000,   
+
+  });
+
+
